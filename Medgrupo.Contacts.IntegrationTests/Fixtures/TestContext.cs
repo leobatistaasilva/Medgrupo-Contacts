1f9ee9d216a90b988fed39d62f7960af02f8cb49
leobatistaasilva <67927387+leobatistaasilva@users.noreply.github.com>
2020-07-06 18:49:08 -0300

    integration tests

---
+++ Medgrupo.Contacts.IntegrationTests/Fixtures/TestContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.Net.Http;
+using Medgrupo.Contacts.Api;
+
+namespace Medgrupo.Contacts.IntegrationTests.Fixtures
+{
+    public class TestContext
+    {
+        public HttpClient Client { get; set; }
+        private TestServer _server;
+
+        public TestContext()
+        {
+            SetupClient();
+        }
+
+        private void SetupClient()
+        {
+            _server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+
+            Client = _server.CreateClient();
+
+        }
+
+        public static void Main(string[] args)
+        {
+            new TestContext();
+            //CreateHostBuilder(args).Build().Run();
+        }
+
+    //    public static void Main(string[] args)
+    //    {
+    //        CreateHostBuilder(args).Build().Run();
+    //    }
+
+    //    public static IHostBuilder CreateHostBuilder(string[] args) =>
+    //        Host.CreateDefaultBuilder(args)
+    //            .ConfigureWebHostDefaults(webBuilder =>
+    //            {
+    //                webBuilder.UseStartup<Startup>()
+    //                .UseUrls("http://*:5015");
+    //            });
+    //}
+
+    }
+}
